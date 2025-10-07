b8e5789d0842e33da69bff692203a208d5b6ee41
draudrichman <88120992+draudrichman@users.noreply.github.com>
2025-10-07 14:13:34 +0600

    logo

--- app/components/store-logos.tsx
+++ app/components/store-logos.tsx
@@ -7,7 +7,7 @@ const ScrollLogos = ({ companies }: any) => {
             className={`marquee__group ${companies?.length > 7 ? " marquee_animated" : ""}`}
           >
             {companies?.map((company: any) => (
-              <div className="bg-white h-38" key={company.name}>
+              <div className="bg-white h-12 sm:h-15 md:h-30 xl:h-40 2xl:h-50" key={company.name}>
                 <img className="h-full" src={company.src} alt={company.name} />
               </div>
             ))}
